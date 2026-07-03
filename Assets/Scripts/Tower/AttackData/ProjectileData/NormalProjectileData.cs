83b76eca223be4e8e9cb5a93de773a15b5373b0e
jinsobak <100740570+jinsobak@users.noreply.github.com>
2026-07-03 14:56:29 +0900

    공격 데이터 SO 리팩토링

---
+++ Assets/Scripts/Tower/AttackData/ProjectileData/NormalProjectileData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Projectile/Normal")]
+public class NormalProjectileData : ProjectileData
+{
+    
+}
---
+++ Assets/Scripts/Tower/AttackData/ProjectileData/ProjectileData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public abstract class ProjectileData : AttackBaseData
+{
+    [Header("Addressable")]
+    public int projectileID;
+    public int hitEffectID;
+    public AssetReferenceGameObject projectilePF;
+    [HideInInspector] public GameObject loadedPrefab { get; set; }
+
+    [Header("기본 정보")]
+    public float projectileSpeed = 10f;
+    public LayerMask targetLayer;
+}
+
